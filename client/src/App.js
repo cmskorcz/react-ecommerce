8bc9cf9feef0e2123a43b77e76ec8c632ae1ea12
Chase Skorczewski <chaseskorey@icloud.com>
2023-04-26 21:47:04 -0400

    added placeholder hero

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Navigation from './components/Navbar';
+import Hero from './components/Hero';
 
 function App() {
   return (
     <div className="App">
       <Navigation />
+      <Hero />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
